f0e48e1e79dc87baf2dd6bfb453e895207e6c7e4
sandro-yaqub-yusuf <sandroyy@gmail.com>
2020-12-26 12:09:04 -0300

    Criação do projeto
    Criação completa do projeto em Javascript com HTML5 e CSS3

---
+++ assets/js/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'./assets/img/pizzas/pizza1.jpg', price:2.19, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'./assets/img/pizzas/pizza2.jpg', price:3.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'./assets/img/pizzas/pizza3.jpg', price:4.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'./assets/img/pizzas/pizza4.jpg', price:3.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Vegetariana', img:'./assets/img/pizzas/pizza5.jpg', price:3.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'./assets/img/pizzas/pizza6.jpg', price:4.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'./assets/img/pizzas/pizza7.jpg', price:5.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
